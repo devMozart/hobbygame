024e40eb74e284289b52d97b88f497bfa182ebc1
Amadeus Hein <amadeus.hein@gmail.com>
2021-08-03 11:12:34 +0200

    Added some temp ref images, init dialogue view

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DialogueView></DialogueView>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DialogueView from './components/dialogue-view/DialogueView.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DialogueView
   }
 }
 </script>
@@ -21,8 +20,11 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/dialogue-view/DialogueView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="DialogueView">Hello World</div>
+</template>
+
+<script>
+export default {
+  name: 'DialogueView',
+}
+</script>
+
+<style scoped>
+.DialogueView {
+    height: 100vh;
+}
+</style>
